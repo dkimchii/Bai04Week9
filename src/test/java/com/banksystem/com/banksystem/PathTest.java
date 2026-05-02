198b4d8974b3d406a579a99643340471a18608be
dkimchii <moon07erkimchi@gmail.com>
2026-05-02 21:53:01 +0700

    Matrix - fix

--- src/test/java/com/banksystem/com/banksystem/PathTest.java
+++ src/test/java/com/banksystem/com/banksystem/PathTest.java
@@ -1,27 +1,28 @@
 package com.banksystem;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Kiểm thử việc tạo đường dẫn cross-platform bằng lớp Path và Paths.
  */
 public class PathTest {
-
   @Test
   public void testHardcodedPath() {
     // Sử dụng File.separator hoặc java.nio.file.Path để tương thích đa nền tảng
     Path path = Paths.get("data", "config.txt");
     File file = path.toFile();
-    // Hoặc dùng File.separator:
-    // String pathString = "data" + File.separator + "config.txt";
-    // File file = new File(pathString);
-    // Tiếp tục các câu lệnh test của bạn
+
+    /*
+     * Su dung File.separator hoac java.nio.file.Path de tuong thich da nen tang:
+     * String pathString = "data" + File.separator + "config.txt";
+     * File file = new File(pathString);
+     */
+
     assertTrue(true);
   }
 }
